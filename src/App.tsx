8716dcc052d56bb6c476030ede1904290207c82c
berkbozok <berk_bozok@live.com>
2023-03-16 00:21:25 -0400

    main page initialized

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,27 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import NavigationMenu from "./components/navigation";
-
-import { Button } from "antd";
+import { PieChartFilled } from "@ant-design/icons";
 
 function App() {
   return (
     <>
       <div className="navigation-side">
         <NavigationMenu />
-
-        <div className="App">MAIN PAGE</div>
+        <div className="page-layout">
+          <div className="main-title">
+            <PieChartFilled className="icon-title" />
+            Macros Calculator
+          </div>
+          <div className="line"></div>
+          <div className="main-page">
+            <h2>Calculate Your Macros</h2>
+            <p>
+              Craft your ideal macronutrient ratio now using our macros
+              calculator
+            </p>
+          </div>
+        </div>
       </div>
     </>
   );
