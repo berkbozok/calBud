a3c4d77556f81fa85aa3b2646f024a730e4cee62
berkbozok <berk_bozok@live.com>
2023-03-17 23:51:20 -0400

    code cleared v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import NavigationMenu from "./components/navigation";
-import { PieChartFilled } from "@ant-design/icons";
 import Macros from "./components/macros-calc";
 
 function App() {
